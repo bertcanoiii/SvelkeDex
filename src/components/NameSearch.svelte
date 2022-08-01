046599f069053d37465e67532ec77bb63fb49c54
Bert Cano <bertcanoiii@gmail.com>
2022-07-31 17:41:05 -0700

    add wip pagination

--- src/components/NameSearch.svelte
+++ src/components/NameSearch.svelte
@@ -14,31 +14,43 @@
                 console.log(data);
                 pokeData.set(data);
                 nextUrl = data.next;
-                prevUrl = data.previous;
+                prevUrl = data.previous
             }).catch(error => {
             console.log(error);
             return[];
         });
     });
 
-    function handleNext(){
+    async function getNextPokemon() {
         pokemonApi = nextUrl;
-
-        getNewPokemon(async () => {
-            fetch(`${pokemonApi}`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    pokeData.set(data);
-                    nextUrl = data.next;
-                    prevUrl = data.previous;
-                }).catch(error => {
-                console.log(error);
-                return[];
-            });
+        fetch(`${pokemonApi}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                pokeData.set(data);
+                nextUrl = data.next;
+                prevUrl = data.previous;
+            }).catch(error => {
+            console.log(error);
+            return[];
         });
+        $pokemonList = $pokemonList;
     }
 
+    async function getPrevPokemon() {
+        pokemonApi = prevUrl;
+        fetch(`${pokemonApi}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                pokeData.set(data);
+                nextUrl = data.next;
+                prevUrl = data.previous;
+            }).catch(error => {
+            console.log(error);
+            return[];
+        });
+    }
 
 </script>
 
@@ -46,15 +58,18 @@
     <div class="flex flex-col sm:flex-row justify-center">
         <h1 class="mt-2 text-2xl font-bold underline underline-offset-2">Search by Name</h1>
     </div>
-    <div class="mt-5 grid grid-cols-3 gap-x-8 sm:gap-x-20 h-auto justify-center duration-500">
-        {#each $pokemonList as pokemon, i}
-            <p>{pokemon.name}</p>
-        {/each}
+    <div class="mt-5  h-auto justify-center duration-500">
+
+            <ol class="grid grid-cols-3 gap-x-8 sm:gap-x-20">
+                {#each $pokemonList as pokemon, i}
+                    <li class="text-xs">{i}: {pokemon.name}</li>
+                {/each}
+            </ol>
     </div>
-    <div class="flex flex-row mt-8 space-x-24 justify-center">
-           <button class="border border-black {prevUrl ? '' : 'disabled'} rounded-lg py-1 w-28 text-center">prev</button>
+    <div class="flex flex-row mt-8 space-x-24 justify-center"   >
+           <button class="border-2 border-black {prevUrl ? '' : 'hidden'} rounded-lg py-1 w-28 text-center" on:click={() => $pokemonList = getPrevPokemon()}>prev</button>
         {#if nextUrl}
-          <button class="border border-black {nextUrl ? '' : 'disabled'} rounded-lg py-1 w-28 text-center" on:click={handleNext}>next</button>
+          <button class="border-2 border-black {nextUrl ? '' : 'hidden'} rounded-lg py-1 w-28 text-center" on:click={() => $pokemonList = getNextPokemon()}>next</button>
         {/if}
     </div>
 </div>
