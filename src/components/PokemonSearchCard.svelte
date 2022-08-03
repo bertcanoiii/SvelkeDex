06ad9f763cd6aea09e8272e0a65ede220980bbee
Bert Cano <bertcanoiii@gmail.com>
2022-08-02 20:42:35 -0700

    update pokecard styling, implement pagination

--- src/components/PokemonSearchCard.svelte
+++ src/components/PokemonSearchCard.svelte
@@ -4,16 +4,16 @@
 
 </script>
 
-<div class="flex">
-    <div class="flex flex-col m-1 items-center bg-blue-100 rounded-xl py-2 drop-shadow-xl">
+<div class="flex group">
+    <div class="flex flex-col m-1 items-center bg-blue-100 rounded-xl py-2 drop-shadow-xl group-hover:bg-blue-800 group-hover:text-white duration-300">
         <!--            Pokemon Card Top-->
-        <div class="flex flex-col items-center">
-            <h3 class="text-lg sm:text-xl md:text-2xl text font-bold text-slate-700 duration-500">{pokemonName}</h3>
+        <div class="flex flex-col items-center ">
+            <h3 class="text-lg sm:text-xl md:text-2xl text font-bold text-slate-700 group-hover:text-white duration-200">{pokemonName}</h3>
         </div>
         <!--            Pokemon Card Body-->
         <div class="flex flex-row justify-center items-center space-x-2 px-2">
 <!--                                <p class="text-slate-700 font-bold">ID:{pokemon.url.substring(34, pokemon.url.length - 1)}</p>-->
-            <img class="bg-blue-200 rounded-xl" src="images/main_sprites/{pokemonId}.png" alt=""/>
+            <img class="bg-blue-200 rounded-xl group-hover:bg-transparent hover:-translate-y-0.5 hover:scale-150 hover:rotate-3 duration-100 group-hover:a" src="images/main_sprites/{pokemonId}.png" alt=""/>
         </div>
     </div>
 </div>
--- src/routes/PokemonSearch.svelte
+++ src/routes/PokemonSearch.svelte
@@ -1,64 +1,75 @@
 <script>
-    // import sprites from "../sprites.js"
-    // import { onMount } from 'svelte';
-    // import { pokeData, pokemonList } from "../store.js";
-    //
-    // onMount(async () => {
-    //     fetch("https://pokeapi.co/api/v2/pokemon")
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             console.log(data);
-    //             pokeData.set(data);
-    //         }).catch(error => {
-    //         console.log(error);
-    //         return[];
-    //     });
-    // });
-
     import pokemonData from "../pokemonData.js";
     import PokemonSearchCard from "../components/PokemonSearchCard.svelte";
 
     const pokemonInitialData = pokemonData;
+    let currentPage = 1;
+    let pokemonPerPage = 24;
+    let allPokemon = pokemonInitialData;
+    let totalPokemon = pokemonInitialData.length;
+    let pageNumbers = 5;
+    $: pokeRangeHigh = currentPage * pokemonPerPage;
+    $: pokeRangeLow = pokeRangeHigh - pokemonPerPage;
+
+    const setCurrentPage = newPage => {
+        currentPage = newPage;
+    }
 
     function handleClick() {
         alert("Clicked!")
     }
 
+
 </script>
 
-<div class="flex flex-col max-w-6xl px-5 devBorder">
-    <h1 class="text-4xl text-slate-700 text-center sm:text-start font-bold drop-shadow-xl duration-200">This is the Pokemon page</h1>
+<div class="flex flex-col max-w-5xl px-5">
+    <h1 class="text-4xl text-slate-700 text-center font-bold drop-shadow-lg duration-200">This is the Pokemon page</h1>
 <!--Pokemon Card Container-->
-    <div class="flex flex-row flex-wrap justify-center sm:justify-start pt-10">
+    <div class="flex flex-row flex-wrap justify-center pt-10 devBorder">
 <!--    Loop for each pokemon card-->
-    {#each pokemonInitialData as pokemon}
+    {#each allPokemon as pokemon, i}
+        {#if i >= pokeRangeLow && i < pokeRangeHigh }
         <!--Pokemon Cards-->
-        <PokemonSearchCard pokemonName="{pokemon.identifier}" pokemonId="{pokemon.id}"></PokemonSearchCard>
+            <PokemonSearchCard pokemonName="{pokemon.identifier}" pokemonId="{pokemon.id}"></PokemonSearchCard>
+        {/if}
     {/each}
     </div>
-    <div class="flex flex-row justify-between px-10 py-4">
-        <button class="flex justify-center
-                       text-white font-semibold
-                       rounded-lg text-lg
-                       bg-blue-700 border-0 py-2 w-28
-                       focus:outline-none
-                       hover:bg-blue-900 hover:-translate-y-0.5
-                       duration-200"
-                on:click={handleClick}
-        >
-            Previous
-        </button>
-        <button class="flex justify-center
-                       text-white font-semibold
-                       rounded-lg text-lg
-                       bg-blue-700 border-0 py-2 w-28
-                       focus:outline-none
-                       hover:bg-blue-900 hover:-translate-y-0.5
-                       duration-200"
-                on:click={handleClick}
-        >
-            Next
-        </button>
+<!--    Navigation Button Containers-->
+    <div class="flex flex-col justify-center devBorder">
+<!--        <div class="flex flex-row justify-center space-x-5 pt-4 text-xl text-slate-700 font-bold devBorder ">-->
+<!--            {#each Array(pageNumbers) as page, i}-->
+<!--                <button class="hover:-translate-y-0.5 duration-300 hover:underline">{i+1}</button>-->
+<!--            {/each}-->
+<!--        </div>-->
+        <div class="flex flex-row justify-between px-10 py-4 space-x-5 devBorder">
+
+            <button class="flex justify-center
+                           text-slate-700 font-semibold
+                           rounded-lg text-lg
+                           bg-blue-200 border-0 py-2 w-28
+                           focus:outline-none
+                           hover:bg-blue-900 hover:-translate-y-0.5 hover:text-white
+                           drop-shadow-lg
+                           duration-200"
+                    on:click|preventDefault={() => setCurrentPage(currentPage - 1)}
+            >
+                Previous
+            </button>
+
+            <button class="flex justify-center
+                           text-slate-700 font-semibold
+                           rounded-lg text-lg
+                           bg-blue-200 border-0 py-2 w-28
+                           focus:outline-none
+                           hover:bg-blue-900 hover:-translate-y-0.5 hover:text-white
+                           drop-shadow-lg
+                           duration-200"
+                    on:click|preventDefault={() => setCurrentPage(currentPage + 1)}
+            >
+                Next
+            </button>
+        </div>
+
     </div>
 </div>
 
