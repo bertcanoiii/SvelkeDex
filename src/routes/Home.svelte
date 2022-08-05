c7a527e0f087798a897e099a950702b8bd1bd9aa
Bert Cano <bertcanoiii@gmail.com>
2022-08-05 14:41:12 -0700

    Adjust homep page

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,6 +1,7 @@
 <script></script>
 
-<div>
-    <h1>This is the home page</h1>
+<div class="p-5">
+    <h1 class="pageTitle">This is the home page</h1>
+    <h1 class="pageTitle">Still Under Construction!</h1>
 
 </div>
