645420251d19c087b39afce0340fc6ab622e043e
Bert Cano <bertcanoiii@gmail.com>
2022-08-05 13:53:49 -0700

    adjust nav bar add logo

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,15 +1,30 @@
 <script>
 </script>
 
-<div class="hidden sm:flex sm:flex-col sm:min-h-screen bg-blue-300 devBorder">
-    <div class="flex flex-col flex-1 overflow-hidden pt-10 px-5 w-36">
+<div class="hidden text-center sm:flex sm:flex-col sm:min-h-screen bg-blue-300 devBorder">
+    <div class="flex flex-col items-center pt-2 px-5 w-36 devBorder">
         <div class="flex">
-            <h1 class="text-slate-700 font-bold text-xl underline">logo placeholder</h1>
+            <a href="/">
+                <img class="devBorder ml-2 hover:rotate-12 duration-300" src="images/logo.png" alt="logo">
+            </a>
         </div>
-        <div class="flex flex-col space-y-10 pt-10 devBorder">
-            <a class="text-slate-700 font-bold text-xl underline" href="#/about">go to about </a>
-            <a class="text-slate-700 font-bold text-xl underline" href="#/pokemon">go to pokemon</a>
-            <a class="text-slate-700 font-bold text-xl underline" href="/">go home</a>
+        <div class="flex flex-col space-y-5 pt-5 devBorder">
+            <a class="navBarLink" href="/">Home</a>
+            <a class="navBarLink" href="#/pokemon">Pokemon</a>
+            <a class="navBarLink" href="#/about">About</a>
         </div>
     </div>
 </div>
+
+
+<style>
+    .navBarLink {
+        @apply
+        text-xl
+        text-slate-700
+        font-bold
+        underline
+        hover:text-white
+        duration-300
+    }
+</style>
