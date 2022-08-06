e13af3dc9822fd113d7e16000c74e1f9715ad6ef
Bert Cano <bertcanoiii@gmail.com>
2022-08-05 18:53:09 -0700

    add pokeball loading

---
+++ src/components/Loading.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { fade } from 'svelte/transition';
+</script>
+
+<div class="relative flex flex-col justify-center devBorder">
+    <div class="min-h-screen -z-10"
+         style="background-image: url('/images/simple_pokeball.gif');
+                   background-repeat: no-repeat;
+                   background-position: top;
+                   background-size: cover;"
+         transition:fade>
+    </div>
+</div>
