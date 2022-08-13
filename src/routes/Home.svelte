d84a1ee36f1b0c8c85889810fa081daf3818a3a0
Bert Cano <bertcanoiii@gmail.com>
2022-08-13 13:15:49 -0700

    global code cleanup, adjust home page styling

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -3,21 +3,24 @@
 </script>
 
 <div class="relative flex flex-col min-h-screen items-center max-w-5xl mx-auto devBorder">
+<!--  Main Inner Container-->
   <div class="flex flex-col items-center justify-center h-screen w-screen">
+<!--    Welcome text-->
     <div class="flex flex-col text-6xl text-center text-slate-700" in:fade={{delay: 900}}>
       <h1>Welcome to</h1>
       <div class="flex flex-row items-center"
            in:slide={{delay: 950, duration: 500}}
       >
         <a href="https://svelte.dev/">
-          <img class="h-20 opacity-80 hover:scale-125 hover:opacity-100 duration-200"
+          <img class="h-20 opacity-80 hover:scale-125 hover:opacity-100 duration-150"
                src="images/svelte-icon.png"
                alt="">
         </a>velkedex!
       </div>
       <p class="text-xs">A web app made with svelte!</p>
     </div>
-    <div class="flex flex-col my-5 px-2 sm:px-0 overflow-y-auto homeText" in:fade={{delay: 1000}}>
+<!--    Gif Links-->
+    <div class="flex flex-col mt-5 mb-10 px-2 sm:px-0 overflow-y-auto homeText" in:fade={{delay: 1000}}>
       <div class="flex flex-col space-y-5 sm:flex-row sm:space-x-5 sm:space-y-0  devBorder"
            in:fade={{delay: 1200}}>
         <a class="flex flex-col items-center justify-center linkBackground devBorder"
@@ -42,13 +45,14 @@
   .linkBackground {
     @apply
     bg-white/30
-    rounded-3xl
+    rounded-lg
     hover:bg-white/90
-    hover:border-slate-900
-    hover:border-8
+    hover:-translate-y-0.5
+    hover:drop-shadow-2xl
+    hover:rounded-2xl
     transition-all
     ease-linear
-    duration-300
+    duration-100
   }
   
   .homeText {
